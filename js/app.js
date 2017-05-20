0220046160563f224a456f55b819eae5731ab41d
neophoenix733 <neophoenix777@gmail.com>
2017-05-19 19:58:54 -0700

    final draft

--- js/app.js
+++ js/app.js
@@ -15,21 +15,22 @@ var state;
 var gifClass;
 var newBtn;
 var searchVal;
-jQuery.fn.exists = function(){ return this.length > 0; }
+
+	
 
 var shows = [
-	'Murder She Wrote',
-	'Star Trek Voyager',
-	'BeastMaster',
+	'24',
+	'Homeland',
+	'Supernatural',
 	'Buffy the Vampire Slayer',
 	'Battlestar Galactica',
-	'Bones',
-	'Quantum Leap',
+	'MXC',
+	'Beavis and Butthead',
 	'CSI',
 	'Mad Men',
 	'Breaking Bad',
 	'The Wire',
-	'Xena Warrior Princess'
+	'House of Cards'
 ], j;
 
 function displayButtons() {
@@ -155,14 +156,3 @@ $(document).ready(function() {
 
 }); // end document ready
 
-// page loads with header and start button
-// on click start button, load first question, start timer set interval
-// if on click answer before timer runs out, freeze timer.  
-	//if correct, display well done photo frozen timer.  
-	// if wrong, display sorry correct answer photo frozen timer.
-	// if timer runs out, display time's up correct answer photo frozen 0 timer.
-// set timeout to load next question and reset timer.
-// loop through all questions until finished.
-	// hide timer
-	// correct answers, incorrect answers, timed out
-	// start over button resets game
